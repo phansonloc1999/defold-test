61a2b9b8be8125c35dca1c632bde849dc9d37a81
phansonloc1999 <phansonloc1999@gmail.com>
2019-12-18 22:52:40 +0700

    Player can respawn with multiple lives

--- Scripts/Constants.lua
+++ Scripts/Constants.lua
@@ -2,6 +2,9 @@
 -- To get access to the functions, you need to put:
 -- require "my_directory.my_file"
 -- in any script using the functions.
-MESSAGE_IDS = {SPAWN_CRATE = "spawn_crate"}
+MESSAGE_IDS = {
+	SPAWN_CRATE = "spawn_crate", SET_HEALTH_FILL_WIDTH = "set_health_bar_fill_width",
+	LOSING_LIFE = "losing_life"
+}
 
 COLLISION_OBJ_GROUPS = { BULLETS = "bullets", PLAYER = "player", ENEMIES = "enemies", CRATES = "crates"}
