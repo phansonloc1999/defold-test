89ff92d67566def2bf6cbbd5723e78f2a255d522
phansonloc1999 <phansonloc1999@gmail.com>
2019-12-20 09:58:36 +0700

    Add meteors

---
+++ Game objects/Meteors/Meteor factories.go
@@ -0,0 +1,69 @@
+components {
+  id: "Meteor factory"
+  component: "/Scripts/Meteor factories.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Big meteor factory"
+  type: "factory"
+  data: "prototype: \"/Game objects/Meteors/Big meteor.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Medium meteor factory"
+  type: "factory"
+  data: "prototype: \"/Game objects/Meteors/Medium meteor.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Small meteor factory"
+  type: "factory"
+  data: "prototype: \"/Game objects/Meteors/Small meteor.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
