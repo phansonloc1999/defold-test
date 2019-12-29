6eea0a3ae063f577163b0ea8e3c4e006954f86ce
phansonloc1999 <phansonloc1999@gmail.com>
2019-12-29 10:36:29 +0700

    Enemy boss in level 2

--- Scripts/Constants.lua
+++ Scripts/Constants.lua
@@ -3,12 +3,16 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 MESSAGE_IDS = {
-	SPAWN_CRATE = "spawn_crate", SET_HEALTH_FILL_WIDTH = "set_health_bar_fill_width",
-	LOSING_LIFE = "losing_life", UPDATE_SCORE = "update_score",
-	SPAWN_GEM = "spawn_gem", LEVEL_ANNOUNCING = "level_announcing",
-	BUBBLE_HIT = "bubble_hit"
+    SPAWN_CRATE = "spawn_crate",
+    SET_HEALTH_FILL_WIDTH = "set_health_bar_fill_width",
+    LOSING_LIFE = "losing_life",
+    UPDATE_SCORE = "update_score",
+    SPAWN_GEM = "spawn_gem",
+    NOTIFY_LEVEL_ANNOUNCER = "notify_level_announcer",
+    INIT_LEVEL_ANNOUNCING = "init_level_announcing",
+    BUBBLE_HIT = "bubble_hit"
 }
 
-COLLISION_OBJ_GROUPS = { BULLETS = "bullets", PLAYER = "player", ENEMIES = "enemies", CRATES = "crates"}
+COLLISION_OBJ_GROUPS = {BULLETS = "bullets", PLAYER = "player", ENEMIES = "enemies", CRATES = "crates"}
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 640, 480
