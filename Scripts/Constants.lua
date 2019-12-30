3211463d05de07594b0828db1dd0981c8a242a88
phansonloc1999 <phansonloc1999@gmail.com>
2019-12-30 20:55:08 +0700

    Minor changes

--- Scripts/Constants.lua
+++ Scripts/Constants.lua
@@ -1,7 +1,3 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
 MESSAGE_IDS = {
     SPAWN_CRATE = "spawn_crate",
     SET_HEALTH_FILL_WIDTH = "set_health_bar_fill_width",
