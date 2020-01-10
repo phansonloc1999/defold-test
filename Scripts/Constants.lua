0c7bf9901d29d5e5aad9fdb2a0f237deb912018a
phansonloc1999 <phansonloc1999@gmail.com>
2020-01-10 22:07:42 +0700

    Minor changes

--- Scripts/Constants.lua
+++ Scripts/Constants.lua
@@ -8,8 +8,7 @@ MESSAGE_IDS = {
     INIT_LEVEL_ANNOUNCING = "init_level_announcing",
     BUBBLE_HIT = "bubble_hit",
     ENEMY_BOSS_HIT = "enemy_boss_hit",
-    ON_AN_ENEMY_DEATH = "on_an_enemy_death",
-    REMOVE_BOSS_FROM_ENEMY_IDS = "remove_boss_from_enemy_ids"
+    ON_AN_ENEMY_DEATH = "on_an_enemy_death"
 }
 
 COLLISION_OBJ_GROUPS = {BULLETS = "bullets", PLAYER = "player", ENEMIES = "enemies", CRATES = "crates"}
